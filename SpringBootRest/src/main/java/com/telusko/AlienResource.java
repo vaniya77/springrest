a1566f1ac604b96a7cd72c0760254e73cce6f5e7
User <User@host.docker.internal>
2021-06-13 16:13:31 +0530

    first commit
    uploading all files.

---
+++ SpringBootRest/src/main/java/com/telusko/AlienResource.java
@@ -0,0 +1,27 @@
+package com.telusko;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlienResource {
+	
+	@Autowired
+	AlienRepository repo;
+	
+	@GetMapping("aliens")
+	public List<Alien> getAliens()
+	{
+		
+		List<Alien> aliens = (List<Alien>) repo.findAll();
+		
+
+		
+		return aliens;
+	}
+}
